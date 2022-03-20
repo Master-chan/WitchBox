8d4c278ff57f727df2dc7c0cadeff5935461c35c
mc <master-chan@mail.ru>
2022-03-20 06:50:11 +0500

    Use InputStream instead of File::readAllBytes cuz we run out of memory

--- src/main/java/org/witch/standalonebox/api/plugin/PluginUpdater.java
+++ src/main/java/org/witch/standalonebox/api/plugin/PluginUpdater.java
@@ -1,14 +1,14 @@
 package org.witch.standalonebox.api.plugin;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.logging.Level;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -32,10 +32,15 @@ public class PluginUpdater
 			URL updateUrl = new URL(updateString);
 			URL sha1Url = new URL(updateUrl.toString() + ".sha1");
 			
-			String localSha1 = DigestUtils.sha1Hex(Files.readAllBytes(Paths.get(jarfile.toURI()))).toLowerCase().trim();
+			String localSha1 = null;
+			try(FileInputStream fis = new FileInputStream(jarfile);
+				BufferedInputStream bis = new BufferedInputStream(fis))
+			{
+				localSha1 = DigestUtils.sha1Hex(bis).toLowerCase().trim();
+			}
 			String remoteSha1 = copyURLToString(sha1Url, StandardCharsets.UTF_8, 2000, 2000).toLowerCase().trim();
-						
-			if(remoteSha1 != null && remoteSha1.length() > 1 && !localSha1.equals(remoteSha1))
+
+			if(remoteSha1 != null && localSha1 != null && remoteSha1.length() > 1 && !localSha1.equals(remoteSha1))
 			{
 				Server.getInstance().getLogger().info(String.format("Updating plugin file %s", jarfile.getName()));
 				FileUtils.copyURLToFile(updateUrl, jarfile, 2000, 2000);
